02ced9e39e67952fb05852e1ae88061da2dfb295
Gopal Programmer <mr.gopal.programmer@gmail.com>
2026-02-06 14:31:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5326345038-d9330",
+  "appId": "1:764996141201:web:f45198dedab1df51183f30",
+  "apiKey": "AIzaSyC2E9S9XFNRDS2y3a3ly0daGkCIpFCAHfw",
+  "authDomain": "studio-5326345038-d9330.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "764996141201"
+};
